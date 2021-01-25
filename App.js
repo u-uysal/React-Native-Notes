794f784301d0d5a39841d7f2db850352b44fd2c7
u-uysal <ufuk.uysl5@gmail.com>
2021-01-25 22:17:17 +0100

    basic installation

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [state, setstate] = useState("ufuk");
+
+  const clickHandler = () => {
+    setstate("uysal");
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{state}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="click me" onPress={clickHandler} />
+      </View>
     </View>
   );
 }
@@ -14,8 +20,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    marginTop: 20,
   },
 });
