f8424372a35efde3c2d8d69e5ff2876e6e9f47e2
u-uysal <ufuk.uysl5@gmail.com>
2021-01-25 22:29:50 +0100

    text input

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
-  const [state, setstate] = useState("ufuk");
+  const [state, setstate] = useState("");
 
-  const clickHandler = () => {
-    setstate("uysal");
+  const inputHandler = (val) => {
+    setstate(val);
   };
   return (
     <View style={styles.container}>
+      <Text>Name</Text>
+      <TextInput
+        multiline //write multiline code
+        style={styles.inputArea}
+        onChangeText={inputHandler}
+        placeholder="your name"
+      />
+      <Text>Age</Text>
+      <TextInput
+        keyboardType="numeric" // write only numbers
+        style={styles.inputArea}
+        onChangeText={inputHandler}
+        placeholder="your name"
+      />
       <Text>{state}</Text>
-      <View style={styles.buttonContainer}>
-        <Button title="click me" onPress={clickHandler} />
-      </View>
     </View>
   );
 }
@@ -24,7 +35,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  buttonContainer: {
-    marginTop: 20,
+  inputArea: {
+    border: "1px solid red",
+    padding: 10,
+    margin: 10,
   },
 });
