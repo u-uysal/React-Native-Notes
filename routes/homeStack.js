048c5902cf44dbedf83991c870c2165b71e02ce9
u-uysal <ufuk.uysl5@gmail.com>
2021-01-27 12:05:59 +0100

    routing

---
+++ routes/homeStack.js
@@ -0,0 +1,31 @@
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import Home from "../screens/home";
+import ReviewDetails from "../screens/reviewDetails";
+
+const screens = {
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      title: "GameZone",
+      //headerStyle: { backgroundColor: '#eee' }
+    },
+  },
+  ReviewDetails: {
+    screen: ReviewDetails,
+    navigationOptions: {
+      title: "Review Details",
+      //headerStyle: { backgroundColor: '#eee' }
+    },
+  },
+};
+
+// home stack navigator screens
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerTintColor: "#444",
+    headerStyle: { backgroundColor: "#eee", height: 60 },
+  },
+});
+
+export default createAppContainer(HomeStack);
